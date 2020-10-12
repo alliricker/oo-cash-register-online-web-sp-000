2ff5353238410d68f65e54414ecc7f81a61b7d6e
Allison Ricker <alliricker@gmail.com>
2020-10-12 00:15:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
 
 
   def add_item(item, price, quantity = 1)
-    @items << ("#{title} " * quantity).split(" ")
+    @items << ("#{item} " * quantity).split(" ")
     @items.flatten!
     @total += price * quantity
     @prev_total = @total
