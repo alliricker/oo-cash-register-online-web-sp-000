2410288fa787f13694a400e9e7d4e41847784b02
Allison Ricker <alliricker@gmail.com>
2020-10-12 00:43:23 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,36 +1,37 @@
 
 class CashRegister
 
-  attr_accessor :total, :discount, :prev_total, :items
+attr_accessor :total, :discount, :items
 
-  def initialize(discount = 0)
-    @total = 0
-    @discount = discount
-    @items = []
-  end
-
-
-  def add_item(item, price, quantity = 1)
-    @items << ("#{item} " * quantity).split(" ")
-    @items.flatten!
-    @total += price * quantity
-    @prev_total = @total
-    @prev_total
+def initialize(discount = 0)
+  @total = 0
+  @discount = discount
+  @items = []
 end
 
 
-  def apply_discount()
-    if @discount > 0
-      @discount = @discount/100.to_f
-      @total = @total - (@total * (@discount))
-      "After the discount, the total comes to $#{@total.to_i}."
-    else
-      "There is no discount to apply."
-    end
+def add_item(item, price, quantity = 1)
+  quantity.times do
+    @items << item
   end
+   @total += price * quantity
+   @prev_total = @total
+   @prev_total
+end
+
+
+def apply_discount
+  @total -= (@total * @discount.to_f / 100).to_i
+  if discount != 0
+    "After the discount, your total is $#{@total}."
+  else
+    "There is no discount to apply."
+  end
+end
+
 
 def void_last_transaction()
-  @total =  self.total - self.prev_total
+  @total = @prev_total
 end
 
 end
